cb3dafe319601b97259344c52a6b56972dab6f3f
echandia27 <jp.echandia@gmail.com>
2026-01-21 21:39:31 -0500

    algunos cambios

--- Practica/login.js
+++ Practica/login.js
@@ -1,6 +1,16 @@
 const loginBtn = document.getElementById("loginBtn");
+const userInput = document.getElementById("username");
+
 
 loginBtn.addEventListener("click", () => {
+    const username = userInput.value.trim().toLowerCase();
+
+    if (username === "") {
+        alert("Debes escribir un usuario");
+        return;
+    }
+
     localStorage.setItem("isLogged", "true");
+    
     window.location.href = "dashboard.html";
 });
